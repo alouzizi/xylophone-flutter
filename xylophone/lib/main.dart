f0c1f98396ed81b270a93dd75728291e4030f710
alouzizi <alouzizi@student.1337.ma>
2023-05-01 20:10:23 +0100

    Building for each note a key and adding an app icon

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main() => runApp(Xylophone());
+void main() => runApp(const Xylophone());
 
 class Xylophone extends StatelessWidget {
   const Xylophone({super.key});
+
+  void playSound(int i) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$i.wav'));
+  }
+
+  Expanded keys(int i, Color color) {
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: () {
+          playSound(i);
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(color),
+        ),
+        child: null,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                keys(1, Colors.red),
+                keys(2, Colors.orange),
+                keys(3, Colors.yellow),
+                keys(4, Colors.green),
+                keys(5, Colors.teal),
+                keys(6, Colors.blue),
+                keys(7, Colors.purple),
+              ]),
         ),
       ),
     );
